e0f5ab37aa57fdddb9f7279faf54a4f6219ee445
nyiri7 <nyiri.levente14@gmail.com>
2026-06-27 08:31:40 +0200

    Added more of my projects

--- aboutme/src/resources/ProjectsData.js
+++ aboutme/src/resources/ProjectsData.js
@@ -13,6 +13,36 @@ export const projectsData = [
     title: "Light Intensity Queue System",
     description: "Basic project for practicing message queues. ActiveMQ was used to send messages between a light intensity sensor and a web application. The application was built using Java.",
     tags: ["Java", "ActiveMQ", "Message Queues"]
+  },
+  {
+    title: "CostTrack",
+    description: "A mobile application built in Java and Kotlin for tracking expenses and managing budgets. It features user-friendly interfaces.",
+    tags: ["Java", "Kotlin", "Mobile"]
+  },
+  {
+    title: "Method of Least Squares",
+    description: "A project that implements the method of least squares for data fitting and regression analysis. It was built using Python and NumPy.",
+    tags: ["Python", "NumPy", "FastAPI"]
+  },
+  {
+    title: "Newton-Halley Method",
+    description: "A project that implements the Newton-Halley method for finding roots of equations. It was built using Python.",
+    tags: ["Python", "FastAPI"]
+  },
+  {
+    title: "Angular Rent Application",
+    description: "A web application built using Angular for managing rental vehicles. It features user authentication and rental management.",
+    tags: ["Angular", "Web Development", "TypeORM"]
+  },
+  {
+    title: "Space Wars Game",
+    description: "A simple 2D space shooter game built using C# WPF. Players can control a spaceship and shoot down enemy ships, they also have the ability to upgrade their weapons.",
+    tags: ["C#", "WPF", "Game Development"]
+  },
+  {
+    title: "Bike webshop",
+    description: "A web application built using natural HTML, CSS and JavaScript for managing a bike webshop. It features product listings, shopping cart functionality, and user authentication.",
+    tags: ["HTML", "CSS", "JavaScript"]
   }
 ];
 
